85dc49cfcbe1e34118d22ece4c8e77464e574e2f
Vincent Lee <vincentlee1776@gmail.com>
2019-10-01 17:07:46 -0400

    hough accumulator complete

--- hw2/hough.py
+++ hw2/hough.py
@@ -2,55 +2,57 @@ import numpy
 import clip 
 import matplotlib.pyplot as plt
 import math
+import imageio
 
 def hough(image, x_buckets, y_buckets):
     H_accumulator = numpy.array([[0 for _ in range(y_buckets)] for _ in range(x_buckets)])
-    x_size, y_size = image.shape
+    theta_intervals = 180 / (y_buckets - 1) 
+    # maximum distance from 0,0
+    max_rho = (image.shape[0] ** 2 + image.shape[1] ** 2) ** (1/2)
+    # * 2 since we need to add all values by x_buckets//2 
+    rho_intervals = max_rho * 2 / (x_buckets - 1) 
     points = []
-    for i in range(x_size-1):
-        for j in range(y_size-1): 
+    for i in range(image.shape[0]):
+        for j in range(image.shape[1]):
             if image[i][j] > 0:
                 points.append([i,j])
-    theta_intervals = 180 / y_buckets
-    arr = []
-    x_axis = []
     for point in points:
-        for i in range(y_buckets):
-            theta = theta_intervals * i
-            print(theta)
-            rho = int(point[1] * numpy.cos(theta) + point[0] * numpy.sin(theta)) + x_buckets//2
-            if rho < x_buckets:
-                H_accumulator[rho][i] += 1
-    #return H_accumulator
-    plt.imshow(H_accumulator,cmap='gray')
-    plt.show()
-    ret_image = numpy.zeros(image.shape)
-    ret_image = extract_line(H_accumulator,ret_image,theta_intervals,x_buckets)
-    return extract_line(H_accumulator,ret_image,theta_intervals,x_buckets)
+        for theta_index in range(y_buckets):
+            theta = theta_index * theta_intervals * numpy.pi / 180
+            # actual rho
+            rho = point[1] * numpy.cos(theta) + point[0] * numpy.sin(theta)
+            # rho index, center it
+            rho_index = rho / rho_intervals + x_buckets // 2
+            H_accumulator[int(rho_index)][theta_index] += 1
+    extractLines(H_accumulator, image, theta_intervals, rho_intervals)
+    return H_accumulator
 
-def extract_line(H_accumulator,image, theta_intervals,x_buckets):
-    max_value = -1 
-    max_row = 0 
-    max_col = 0
-    x_size, y_size = H_accumulator.shape
-    for i in range(x_size):
-        for j in range(y_size):
+def extractLines(H_accumulator, image, theta_intervals, rho_intervals):
+    max_value = -1
+    max_x = 0 
+    max_y = 0
+    for i in range(H_accumulator.shape[0]):
+        for j in range(H_accumulator.shape[1]):
             if H_accumulator[i][j] > max_value:
                 max_value = H_accumulator[i][j]
-                max_row = i
-                max_col = j
-    theta = max_col * theta_intervals
-    slope = -numpy.cos(theta) / numpy.sin(theta) 
-    intercept = math.ceil((max_row-(x_buckets//2)) / numpy.sin(theta))
-    H_accumulator[max_row][max_col] = 0
-    print(max_row, slope,intercept, theta, max_row)
-    draw_line(image,slope,intercept)
-    return image
+                max_x = i
+                max_y = j
+    theta = max_y * theta_intervals * numpy.pi / 180
+    rho = (max_x - H_accumulator.shape[0]//2) * rho_intervals
+    intercept = rho * numpy.sin(theta)
+    
+    print(max_x,max_y,theta,rho,intercept) 
 
+def drawline(image,slope,intercept):
+    
+    return None
 
-def draw_line(image,slope,intercept):
-    x_size, y_size = image.shape
-    for i in range(x_size):
-        target = slope * i + intercept 
-        if target >= 0 and target < y_size: 
-            image[i][int(target)] = 255
+if __name__ == "__main__":
+    image = imageio.imread('canny.gif')
+    print(image.shape)
+    x = 11
+    #image = numpy.array([[0 for _ in range(x)]for _ in range(x)])
+    #image[5][5] = 255
+    image = hough(image,360,360)
+    plt.imshow(image,cmap='gray')
+    plt.show()
--- hw2/main.py
+++ hw2/main.py
@@ -8,48 +8,20 @@ from ransac import ransac
 from hough import hough
 
 if __name__ == "__main__":
-    """
     image = imageio.imread('road.png')
     # smoothing before any operation is done
     image = convulveGaussian(image,1)
     # hessian determinant
     image = hessian.hessianDeterminant(image)
-    plt.imshow(image, cmap='gray')
-    plt.show()
     # threshold the values chose 30 as that seems to remove the most gray points
     image = hessian.threshold(image,40)
     # perform non max suprression on 3x3 surrounding pixels
     image = hessian.nonMaxSuppression(image)
-    imageio.imwrite('hessian.png',image)
     # ransac on threshold 2 and minimum of 16 inliers
-    """
-    image = imageio.imread('hessian.png')
-    
-    image = numpy.array([[0 for _ in range(101)] for _ in range(101)])
-    for i in range(0,101, 1):
-       image[i][i] = 1
-       image[i][100-i] = 1
-    
-    #image[5][5] = 255
-    plt.imshow(image, cmap='gray')
-    plt.show()  
-    hough_image = hough(image,200,200)
+    ransac_image = ransac(image, 2, 15)
+    plt.imshow(ransac_image,cmap='gray')
+    plt.show()
+    # hough with parameters size in the x direction and size in y
+    hough_image = hough(image,2000,720)
     plt.imshow(hough_image, cmap='gray')
     plt.show()  
-    
-    """
-    #ransac_image = ransac(image, 2, 15)
-    image = numpy.array([[0 for _ in range(180)] for _ in range(180)])
-    for i in range(0,180, 1):
-        image[i][i] = 1
-        image[-i][i] = 1
-        image[1][i] = 1
-        image[i][1] = 1
-        
-    #image[5][5] = 255
-    ransac_image = ransac(image, 1, 15)
-    plt.imshow(image,cmap='gray')
-    plt.show()
-    plt.imshow(ransac_image, cmap='gray')
-    plt.show()
-    """
