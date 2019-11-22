284d7798e5b8e5d4295c923caf578793c2077e53
Vincent Lee <vincentlee1776@gmail.com>
2019-11-22 13:12:35 -0500

    started pixel Classification

--- hw4/pixelClassification.py
+++ hw4/pixelClassification.py
@@ -6,6 +6,13 @@ import matplotlib.pyplot as plt
 import heapq
 from sklearn.cluster import KMeans
 
-def 
+def seperateSets(trainingImage,mask):
+    sky = []
+    nonSky = []
+    print(mask.shape)
+    print(trainingImage.shape)
 
 if __name__ == "__main__":
+    mask = imageio.imsave("data/sky/sky_train_gimp.jpg")
+    trainingImage = imageio.imsave("data/sky/sky_train.jpg")
+    seperateSets(trainingImage,mask)
